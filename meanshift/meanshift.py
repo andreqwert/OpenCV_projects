41ab596317cc1d5e65186b33cb9bc97617f2e595
Andrey <timebird@yandex.ru>
2018-05-04 05:45:09 +0300

    Update meanshift.py

--- meanshift/meanshift.py
+++ meanshift/meanshift.py
@@ -10,15 +10,18 @@ ret, frame = cap.read()
 r, h, c, w = 250, 90, 400, 125
 track_window = (c, r, w, h)
 
-"""Set up the ROI for tracking"""
+"""Set up the region of interest for tracking"""
 roi = frame[r:r+h, c:c+w]
+"""..."""
+"""Конвертирует цвета в диапазоне от (0, 60, 32) до (180, 255, 255)"""
 hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180])
-cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+"""..."""
+roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0, 180]) # построение гистограммы
+cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX) # нормировка изображения до диапазона [0, 1]
 
 """Setup the termination criteria, either 10 iteration or move by atlear 1 pt"""
-term_crit = (cv2. TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1) # stop the iteration if specified accuracy, epsilon, is reached
 
 while True:
     ret, frame = cap.read()
@@ -46,3 +49,6 @@ while True:
 
 cv2.destroyAllWindows()
 cap.release()
+
+
+
